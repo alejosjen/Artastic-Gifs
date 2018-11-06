e01d43f3c214b603c33168c0c91681cd4e928120
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-11-05 21:16:08 -0700

    JS ties a button click to an api search, returns results on html in JSON string and in console as array.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,37 @@
 //https://api.giphy.com/v1/gifs/search?api_key=uuAMa4TeItKBBDdRzNb2adG75qTgblof&q=art&limit=10&offset=0&rating=G&lang=en
 
 
-$(function (){
-   var topics = ["art", "art-history", "fine-arts", "architecture", "color", "comics", "modern-art", "sculpture", "painting", "ceramics", "drawing", "watercolor", "chalk-drawing", "abstract-art", "landscape", "photography", "still-life", "mixing-paint", "street-art", "nature-art", "mosaic-art"];
+$(function () {
+    var topics = ["art", "art-history", "fine-arts", "architecture", "color", "comics", "modern-art", "sculpture", "painting", "ceramics", "drawing", "watercolor", "chalk-drawing", "abstract-art", "landscape", "photography", "still-life", "mixing-paint", "street-art", "nature-art", "mosaic-art"];
 
-    $.each(topics, function (index, value){
-        var topicButtons = topics;
-        $(".search-terms").append('<button>' +value+ '</button>');
+    $.each(topics, function (index, value) {
+        $(".search-terms").append('<button type="button" class="topic-button btn btn-dark btn-sm m-1">' + value + '</button>');
         console.log(value);
-    }); 
+    });
 })
+
+var apiKey = "uuAMa4TeItKBBDdRzNb2adG75qTgblof"
+$(document).ready(function () {
+    $('button').on("click", function () {
+        var searchTerm = $(this).text();
+        var url = "https://api.giphy.com/v1/gifs/search?" +
+            `api_key=${apiKey}` +
+            `&q=${searchTerm}` +
+            `&limit=10` +
+            `&rating=G`+
+            `&lang=en`;
+
+        $.ajax({
+            url: url,
+            method: "GET",
+        }).then(function (response) {
+            console.log(response);
+            $(".search-results").text(JSON.stringify(response));
+        })
+    });
+})
+
+
+
+
 
