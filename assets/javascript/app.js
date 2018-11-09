b524b2f47d60df0999fc5316a66a7831b093e73a
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-11-09 16:56:08 -0700

    Added input fields for accepting user search terms and a submit button. Not operational yet, but styled and included in HTML. Rating added. createButtons function created to tie together user input later.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,13 +8,20 @@ document.addEventListener('DOMContentLoaded', function () {
     clear();
 
     $(function () {
-        var topics = ["fine-arts", "architecture", "color", "comics", "sculpture", "painting", "ceramics", "drawing", "watercolor", "chalk-drawing", "abstract-art", "landscape", "photography", "still-life", "mixing-paint", "street-art", "nature-art", "mosaic-art"];
+
+        function createButtons (){
+           var topics = ["fine-arts", "architecture", "color", "comics", "sculpture", "painting", "ceramics", "drawing", "watercolor", "chalk-drawing", "abstract-art", "landscape", "photography", "still-life", "mixing-paint", "street-art", "nature-art", "mosaic-art"];
 
         $.each(topics, function (index, value) {
             $(".search-terms").append('<button type="button" class="topic-button btn btn-dark btn-sm m-1">' + value + '</button>');
             console.log(value);
-        });
+        }); 
+        }
+        
+        createButtons();
 
+
+        
 
         var apiKey = "uuAMa4TeItKBBDdRzNb2adG75qTgblof"
         $('.topic-button').on("click", function () {
@@ -38,32 +45,25 @@ document.addEventListener('DOMContentLoaded', function () {
                     var imageOriginal = response.data[index].images.original.url;
                     var imgResult = $(`
                         <div class = "imageResult">
-                            <img class="gif" src="${imageStill}" alt="Static Image" data-src="${imageOriginal}"/>
-                            Rating G
+                            <img class="gif w-75" src="${imageStill}" alt="Static Image" data-src="${imageOriginal}"/>
+                            <p class="text-light"> Rating: ${response.data[index].rating} </p>
                         </div>
                     `)
                     var img = imgResult.find('.gif')
 
                     imgResult.appendTo(".search-results");
 
-                    img.on("click", function() {
+                    img.on("click", function () {
                         if (img.attr('src') == imageStill) {
                             img.attr('src', imageOriginal)
                         } else if (img.attr('src') == imageOriginal) {
                             img.attr('src', imageStill)
                         }
+                    });
 
-                    })
+                });
+            });
+        });
 
-                    
-
-
-                })
-
-            })
-
-
-        })
     });
-
-})
+});
