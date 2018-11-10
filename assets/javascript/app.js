6bd44e2e57ef4e1d2ebb2db1323477000c5174b3
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-11-10 09:29:56 -0700

    Double check commit, make sure everything's up to date.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
-// API to Giphy: uuAMa4TeItKBBDdRzNb2adG75qTgblof, not a production key
-//https://api.giphy.com/v1/gifs/search?api_key=uuAMa4TeItKBBDdRzNb2adG75qTgblof&q=art&limit=10&offset=0&rating=G&lang=en
+
 var topics = ["fine-arts", "architecture", "color", "comics", "sculpture", "painting", "ceramics", "drawing", "watercolor", "chalk-drawing", "abstract-art", "landscape", "photography", "still-life", "mixing-paint", "street-art", "nature-art", "mosaic-art"];
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -16,15 +15,14 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.log(value);
             });
         }
+        
+        createButtons();
 
         $('form').keypress(function (event) {
             return event.keyCode != 13;
         });
 
-        createButtons();
-
-
-        $('#submit').on('click', function (event) {
+        $('#submit').on('click', function () {
             $(".search-terms").empty()
             var submitButton = $('#art-input').val()
 
@@ -35,14 +33,10 @@ document.addEventListener('DOMContentLoaded', function () {
             }
 
             createButtons();
-            
-
-/*             event.preventDefault();
- */            console.log(topics);
         });
 
         var apiKey = "uuAMa4TeItKBBDdRzNb2adG75qTgblof"
-        $(document).on("click", '.topic-button', function (event) {
+        $(document).on("click", '.topic-button', function () {
 
             var searchTerm = $(this).text();
             var url = "https://api.giphy.com/v1/gifs/search?" +
@@ -58,32 +52,37 @@ document.addEventListener('DOMContentLoaded', function () {
             }).then(function (response) {
                 console.log(response);
                 clear();
-
-                response.data.forEach(function (data, index) {
-                    var imageStill = response.data[index].images.original_still.url;
-                    var imageOriginal = response.data[index].images.original.url;
-                    var imgResult = $(`
-                        <div class = "imageResult">
-                            <img class="gif w-75" src="${imageStill}" alt="Static Image" data-src="${imageOriginal}"/>
-                            <p class="text-light"> Rating: ${response.data[index].rating} </p>
+                
+                    response.data.forEach(function (data, index) {
+                        var imageStill = response.data[index].images.original_still.url;
+                        var imageOriginal = response.data[index].images.original.url;
+                        var imgResult = $(`
+                        <div class="imageResult text-white-50 bg-dark">
+                            <img class="gif w-100" src="${imageStill}" alt="Static Image" data-src="${imageOriginal}"/>
+                            <div> Title: ${response.data[index].title} </div>
+                            <div> URL: <a href="${response.data[index].bitly_url}" class="text-white-50"> ${response.data[index].bitly_url} </a></div>
+                            <div> Rating: ${response.data[index].rating} </div>
+                            <br />
                         </div>
                     `)
-                    var img = imgResult.find('.gif')
+                        var img = imgResult.find('.gif')
 
-                    imgResult.appendTo(".search-results");
+                        imgResult.appendTo(".search-results");
 
-                    img.on("click", function () {
-                        if (img.attr('src') == imageStill) {
-                            img.attr('src', imageOriginal)
-                        } else if (img.attr('src') == imageOriginal) {
-                            img.attr('src', imageStill)
-                        }
+                        img.on("click", function () {
+                            if (img.attr('src') == imageStill) {
+                                img.attr('src', imageOriginal)
+                            } else if (img.attr('src') == imageOriginal) {
+                                img.attr('src', imageStill)
+                            }
+                        });
+
                     });
+                
+                
 
-                });
             });
-/*         event.preventDefault();
- */        });
+        });
 
     });
 });
